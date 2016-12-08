aacd6e46021df15e7ebd33c8ada99cd514da2db6
ercekal <ercekalabalikoglu@gmail.com>
2016-12-08 11:38:42 +0000

    makes the active video list

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-const API_KEY = 'AIzaSyDG9H83lykRsu_KxgxZlizo5818FJt8C9I'
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyDG9H83lykRsu_KxgxZlizo5818FJt8C9I';
 
 
 
@@ -11,17 +12,27 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = { videos: [] }
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
   render() {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
